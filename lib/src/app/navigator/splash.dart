0209ed6943327eb97b0f9ac434b646f32da85b39
solehfuddin <solehfuddin@gmail.com>
2022-02-21 17:10:55 +0700

    Fixed updater check

--- lib/src/app/navigator/splash.dart
+++ lib/src/app/navigator/splash.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
+import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
 import 'package:in_app_update/in_app_update.dart';
 import 'package:sample/src/app/pages/admin/admin_view.dart';
 import 'package:sample/src/app/pages/home/home_view.dart';
 import 'package:sample/src/app/pages/login/login_view.dart';
-import 'package:sample/src/app/utils/custom.dart';
 import 'package:sample/src/app/widgets/newintro.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,43 +17,55 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   AppUpdateInfo _updateInfo;
 
-  Future checkIntro() async{
+  Future checkIntro() async {
     SharedPreferences pref = await SharedPreferences.getInstance();
     bool _check = (pref.getBool('check') ?? false);
     bool _isLogin = (pref.getBool('islogin') ?? false);
 
     if (_check) {
-      if (_isLogin)
-      {
+      if (_isLogin) {
         String role = pref.getString("role");
 
         if (role == 'admin') {
           Navigator.of(context).pushReplacement(
               MaterialPageRoute(builder: (context) => AdminScreen()));
-        } 
-        else if(role == 'sales') {
+        } else if (role == 'sales') {
           Navigator.of(context).pushReplacement(
               MaterialPageRoute(builder: (context) => HomeScreen()));
+        } else {
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => Login()));
         }
-        else {
-          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Login()));
-        }
-      }      
-      else {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Login()));
+      } else {
+        Navigator.of(context)
+            .pushReplacement(MaterialPageRoute(builder: (context) => Login()));
       }
-    }
-    else
-    {
+    } else {
       // await pref.setBool('check', true);
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => NewIntro()));
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => NewIntro()));
     }
   }
 
+  void _showError(dynamic exception) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(exception),
+      duration: Duration(seconds: 2),
+    ));
+  }
+
   void checkUpdate() {
-    if(_updateInfo.updateAvailable == true)
-    {
-      InAppUpdate.performImmediateUpdate().catchError((e) => handleStatus(context, e.toString(), false));
+    if (Platform.isAndroid) {
+      InAppUpdate.checkForUpdate().then((info) {
+        setState(() {
+          _updateInfo = info;
+
+          if (_updateInfo.updateAvailable == true) {
+            InAppUpdate.performImmediateUpdate()
+                .catchError((e) => _showError(e.toString()));
+          }
+        });
+      }).catchError((e) => _showError(e.toString()));
     }
   }
 
@@ -66,8 +78,6 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
